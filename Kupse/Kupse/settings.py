c857099fc9f30daa5078f36d06e8f7aca3f8a59d
Michal K <michalin@DESKTOP-AV3JK51.localdomain>
2022-02-11 15:58:17 +0100

    Debug mode is False.

--- Kupse/Kupse/settings.py
+++ Kupse/Kupse/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'wun-s_u_(87n0fel$4#-wj%y8$cdnivb&re79dy*tmslik*wtz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
